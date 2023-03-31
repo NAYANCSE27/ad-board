db752c6ce81ef52c2854b2b83b87ed0b822ef2cf
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-03-31 12:52:12 +0600

    added basic structure of the project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import "./App.css";
+
 function App() {
+  const activeMenu = true;
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4 style={{zIndex: '1000'}}">
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3
+                hover:drop-shadow-xl
+                hover:bg-light-gray
+                text-white"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div
+              className="w-72 fixed sidebar
+              dark:bg-secondary-dark-bg
+              bg-white"
+            >
+              Sidebar
+            </div>
+          ) : (
+            <div
+              className="w-0 fixed sidebar
+              dark:bg-secondary-dark-bg"
+            >
+              Sidebar w-0
+            </div>
+          )}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full
+              ${activeMenu ? "md:ml-72" : "flex-2"}`}
+          >
+            
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
