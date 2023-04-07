a3c437afa6a423e72eb30a6b378b8b6d5ccabaf8
NAYANCSE27 <mehadihasannayan1998@gmail.com>
2023-04-07 16:37:15 +0600

    added sidebar in the project

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
-import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
-import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import {
+  Ecommerce,
+  Orders,
+  Calendar,
+  Employees,
+  Stacked,
+  Pyramid,
+  Customers,
+  Kanban,
+  Line,
+  Area,
+  Bar,
+  Pie,
+  Financial,
+  ColorPicker,
+  ColorMapping,
+  Editor,
+} from "./pages";
 
 import "./App.css";
 
 function App() {
-  const activeMenu = false;
+  const activeMenu = true;
 
   return (
     <div>
